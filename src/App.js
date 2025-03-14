c91e34f413b24061c2e4928e430595eaf26b1501
matsusht24 <matsusht24@up.edu>
2025-03-13 18:24:06 -1000

    implemented routing and corresponding dependency added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 
 import './App.css';
 import Header from './semantics/Header';
-import Main from './semantics/Main';
 import Footer from './semantics/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/BookingPage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Main />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/booking" element={<BookingPage />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero_Section from "../sections/Hero_Section";
+import Highlights from "../sections/Highlights";
+
+
+function HomePage() {
+  return (
+    <main>
+      <Hero_Section />
+      <Highlights />
+    </main>
+  );
+}
+
+export default HomePage;
