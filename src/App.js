3713c4bdfd887c4a25c68acfd6a1438ee72231ce
matsusht24 <matsusht24@up.edu>
2025-03-19 17:07:05 -1000

    added fluid acess to Booking dates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
-
-import './App.css';
-import Header from './semantics/Header';
-import Footer from './semantics/Footer';
+import "./App.css";
+import Header from "./semantics/Header";
+import Footer from "./semantics/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BookingPage from "./pages/BookingPage";
-import { useReducer } from 'react';
+import { useReducer } from "react";
 
 function App() {
-  const [availableTimes, dispatch] = useReducer(initializeAvailableTimes, updateAvailableTimes);
   const updateAvailableTimes = (selectedTime) => {
     // This function removes the selected time from the available times
-    const newTimes = availableTimes.filter(time => time !== selectedTime);
-    setAvailableTimes(newTimes);
-  }
+    const newTimes = availableTimes.filter((time) => time !== selectedTime);
+    return newTimes;
+  };
   const initializeAvailableTimes = () => {
     // This function resets the available times to the original list
-    setAvailableTimes(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
-  }
+    const intialTimes = [
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+    ];
+   dispatch({ type: "SET_TIMES", payload: intialTimes });
+    return intialTimes;
+  };
+  const [availableTimes, dispatch] = useReducer(
+    initializeAvailableTimes,
+    updateAvailableTimes
+  );
+  
   return (
     <Router>
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/booking" element={<BookingPage availableTimes={availableTimes} setAvailableTimes={dispatch}/>} />
+          <Route
+            path="/booking"
+            element={
+              <BookingPage
+                availableTimes={availableTimes}
+                setAvailableTimes={dispatch}
+              />
+            }
+          />
         </Routes>
       </main>
       <Footer />
